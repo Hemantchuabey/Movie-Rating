12dac3047f70c39e220c1aeb9e3aabb4712a8b8a
Hemant <hementchaubey@gmail.com>
2023-01-27 18:49:08 +0530

    using redux toolkit created store and movieslice which will be helping us to display the movie details in the home page.To display content the changes are done in movieListing and movieCard component and applied respective css to them.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,17 +2,21 @@ import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../Common/API/MovieApi";
 import { movieApiKey } from "../../Common/API/MovieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../Features/Movies/movieSlice";
 
 const Home = () => {
+  const movieText = "War";
+  const dispatch = useDispatch();
   useEffect(() => {
     const fetchMovie = async () => {
-      const movieText = "War";
       const response = await movieApi
         .get(`?apiKey=${movieApiKey}&s=${movieText}&type=movie`)
         .catch((err) => {
           console.log(err);
         });
       console.log("Response from API  : ", response);
+      dispatch(addMovies(response.data));
     };
     fetchMovie();
   }, []);
--- src/Components/MovieListing/MovieListing.js
+++ src/Components/MovieListing/MovieListing.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getAllMovies } from "../../Features/Movies/movieSlice";
+import MovieCard from "../MovieCard/MovieCard";
+import "./MovieListing.scss";
 
 const MovieListing = () => {
-  return <div>MovieListing</div>;
+  const movies = useSelector(getAllMovies);
+  let renderMovies = "";
+  renderMovies =
+    movies.Response === "True" ? (
+      movies.Search.map((movie, index) => {
+        return <MovieCard key={index} data={movie} />;
+      })
+    ) : (
+      <div className="movie-error">
+        <h3>{movies.error}</h3>
+      </div>
+    );
+  console.log("Movies Listing", movies);
+  return (
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container">{renderMovies}</div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieListing;
